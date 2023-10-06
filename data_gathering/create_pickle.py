f5bfbbc38441fc65828c8da677553e9be587602a
Deepak Singh Rautela <114864421+deepak-156@users.noreply.github.com>
2023-10-06 12:10:22 +0530

    create_pickle.py

--- data_gathering/create_pickle.py
+++ data_gathering/create_pickle.py
@@ -8,17 +8,12 @@ import re
 import os
 from PIL import Image
 
-# Script config
-RESIZE_IMAGE = True  # resize the images and write to 'resized_images/'
-GRAYSCALE = True  # convert image to grayscale? this option is only valid if RESIZE_IMAGE==True (FIXME)
-TARGET_W, TARGET_H = 400, 260  # 1.74 is weighted avg ratio, but 1.65 aspect ratio is close enough (1.65 was for stop signs)
 
-###########################
-# Execute main script
-###########################
+RESIZE_IMAGE = True 
+GRAYSCALE = True 
+TARGET_W, TARGET_H = 400, 260 
 
-# First get mapping from sign name string to integer label
-sign_map = {'stop': 1, 'pedestrianCrossing': 2}  # only 2 sign classes (background class is 0)
+sign_map = {'stop': 1, 'pedestrianCrossing': 2} 
 '''
 sign_map = {}  # sign_name -> integer_label
 with open('signnames.csv', 'r') as f:
@@ -28,38 +23,38 @@ with open('signnames.csv', 'r') as f:
 		sign_map[sign_name] = int(integer_label)
 '''
 
-# Create raw data pickle file
+
 data_raw = {}
 
-# For speed, put entire contents of mergedAnnotations.csv in memory
+
 merged_annotations = []
 with open('mergedAnnotations.csv', 'r') as f:
 	for line in f:
 		line = line[:-1]  # strip trailing newline
 		merged_annotations.append(line)
 
-# Create pickle file to represent dataset
+
 image_files = os.listdir('annotations')
 for image_file in image_files:
-	# Find box coordinates for all signs in this image
+	
 	class_list = []
 	box_coords_list = []
 	for line in merged_annotations:
 		if re.search(image_file, line):
 			fields = line.split(';')
 
-			# Get sign name and assign class label
+			
 			sign_name = fields[1]
 			if sign_name != 'stop' and sign_name != 'pedestrianCrossing':
-				continue  # ignore signs that are neither stop nor pedestrianCrossing signs
+				continue
 			sign_class = sign_map[sign_name]
 			class_list.append(sign_class)
 
-			# Resize image, get rescaled box coordinates
+			
 			box_coords = np.array([int(x) for x in fields[2:6]])
 
 			if RESIZE_IMAGE:
-				# Resize the images and write to 'resized_images/'
+				
 				image = Image.open('annotations/' + image_file)
 				orig_w, orig_h = image.size
 
@@ -85,7 +80,7 @@ for image_file in image_files:
 			box_coords_list.append(box_coords)
 
 	if len(class_list) == 0:
-		continue  # ignore images with no signs-of-interest
+		continue 
 	class_list = np.array(class_list)
 	box_coords_list = np.array(box_coords_list)
 
